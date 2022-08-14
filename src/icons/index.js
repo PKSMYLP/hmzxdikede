fd54178d6b6d9225ed6d5e6ece61f782fae27894
jokerp <pks17355914356@163.com>
2022-08-14 16:22:03 +0800

    项目初始化

---
+++ src/icons/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import SvgIcon from '@/components/SvgIcon'// svg component
+
+// register globally
+Vue.component('svg-icon', SvgIcon)
+
+const req = require.context('./svg', false, /\.svg$/)
+console.log(req.keys()) // 导出所有路径
+console.log(req('./dashboard.svg')) // 根据路径 找到对应的模块
+const requireAll = requireContext => requireContext.keys().map(requireContext)
+console.log(requireAll(req))
